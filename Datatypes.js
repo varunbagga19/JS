151cd312df00b63da4065908b26134bbe0a3228d
varunbagga19 <varunbagga19@gmail.com>
2023-07-23 12:38:58 +0530

    first commit

---
+++ Datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS Code as newer version of JS
+
+// alert("hello"); we are using nodejs, not browser
+
+console.log(3 + 3);
+
+
+let name = "varun"
+
+let age = 14
+
+let isLoggedIn = false
+
+// number => 2^53
+// bigint
+// string =>" ",' '
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol
+
+
+
+// object
+
+console.log(typeof "hot");
+console.log(typeof null); // object
+console.log(typeof undefined);
---
+++ conversionOperations.js
@@ -0,0 +1,8 @@
+let score = "33asd";
+
+console.log(typeof score);
+
+let value = Number(score);
+
+console.log(typeof value);
+console.log(value)
