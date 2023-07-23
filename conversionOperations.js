67f4d7d50f2327379a77769fadcb65f9064a989b
varunbagga19 <varunbagga19@gmail.com>
2023-07-23 12:59:45 +0530

    conversionOperations

--- conversionOperations.js
+++ conversionOperations.js
@@ -1,8 +1,41 @@
-let score = "33asd";
+// let score = "33asd";
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let value = Number(score);
+// let value = Number(score);
 
-console.log(typeof value);
-console.log(value)
+// console.log(typeof value);
+// console.log(value)
+
+// //"33" =>33
+// //"33asd" => NaN
+// //true=>1,false=>0
+
+// ************************ Operations ************************
+// let value = 3;
+// let negValuee = -value;
+
+// console.log(negValuee);
+
+
+// let str1 = 'hello'
+// let str2 = ' varun'
+
+// let str3 = str1 + str2;
+
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2  + 2);
+// console.log(1 + 2  + "2");
+
+console.log(+true);
+console.log(typeof (+"5"));
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2;
+
