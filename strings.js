471efa3972c8971ee0f8c7e1a1de10e2f311f490
varunbagga19 <varunbagga19@gmail.com>
2023-07-23 15:46:17 +0530

    Strings

---
+++ strings.js
@@ -0,0 +1,23 @@
+const name = 'varun'
+const repoCount = 50
+
+// console.log(typeof (name + repoCount));
+
+console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+
+const game =  new String('varun');
+console.log(game[0]);
+// console.log(game.__proto__);
+
+
+console.log(game.length);
+console.log(game.toUpperCase());
+
+const newStringOne = "   varun    ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = 'varunbaggais a bad boy'
+console.log(url.replace('bad','good'));
+console.log(url.split(' '));
+
